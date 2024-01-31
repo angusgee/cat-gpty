75c05ea3528c0c065b1e44f02cf1060643f46102
angusgee <maltawebdev@gmail.com>
2024-01-31 09:07:54 +0100

    Add function to read files

--- main.py
+++ main.py
@@ -5,7 +5,10 @@ extensions = [
     '.git',
     '.gitignore', 
     '.env', 
-    '.exe'
+    '.exe',
+    '.jpeg',
+    '.jpg', 
+    '.png'
 ]
 
 # list all files in the current dir and subdirs
@@ -21,18 +24,32 @@ def list_files(dir):
             file_list.append(os.path.join(root, file))
     return file_list
 
+# exclude any files if their extension is in extensions list
 def clean_files(files):
     return [file for file in files if not any(file.endswith(ext) for ext in extensions)]
 
-def count_tokens():
-    return 0
+# read files 
+def read_files(file_path):
+    try:
+        with open(file_path, 'r') as r:
+            return (len(r.read().split()))
+    except Exception as e:
+        print(f'Error reading {file_path}: {e}')
+        return ''
+        
+
+# roughly count the tokens
+# to-do: split longer words into tokens based on five letters per token
+# to-do: also split based on '(' and ')' 
+def count_tokens(file):
+    return len(file.split())
 
 def main():
     dir = os.getcwd()
     files = list_files(dir)
-    cleaned_files = clean_files(files)
-    for file in cleaned_files:
-        print(file)
+    cleaned_file_paths = clean_files(files)
+    for file in cleaned_file_paths:
+        print(read_files(file))
 
 if __name__ == '__main__':
     main()
