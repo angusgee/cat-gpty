cbcdb5bcbe06158d6b4069fc2aca934f837168d8
angusgee <maltawebdev@gmail.com>
2024-01-31 08:15:47 +0100

    Main function
    - add main function
    - add logic to list_files function
    - add logic to ignore .git folder
    - script now gets all files and prints them

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 import os
 
-pwd = os.getcwd()
+def list_files(dir):
+    file_list = []
+    for root, dirs, files in os.walk(dir):
+        if '.git' in dirs:
+            dirs.remove('.git')
+        for file in files:
+            file_list.append(os.path.join(root, file))
+    return file_list
 
-print(pwd)
+def main():
+    dir = os.getcwd()
+    files = list_files(dir)
+    for file in files:
+        print(file)
+
+if __name__ == '__main__':
+    main()
+    
