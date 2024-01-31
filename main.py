d22441175c65f1168bd65dd12afa5c642f49d9e8
angusgee <maltawebdev@gmail.com>
2024-01-31 08:52:11 +0100

    Refactor code
    1. change list comprehension back to for loop as easier to understand.
    2. move settings.extensions back inside main.py.
    3. add clean_files and count_tokens functions.

--- main.py
+++ main.py
@@ -1,13 +1,37 @@
 import os
-import settings 
 
+# customisable list of file extensions to ignore
+extensions = [
+    '.git',
+    '.gitignore', 
+    '.env', 
+    '.exe'
+]
+
+# list all files in the current dir and subdirs
+# exclude pycache and .git folders
 def list_files(dir):
-    return [os.path.join(root, file) for root, dirs, files in os.walk(dir) if '.git' in dirs for file in files]
+    file_list = []
+    for root, dirs, files in os.walk(dir):
+        if '.git' in dirs:
+            dirs.remove('.git')
+        if '__pycache__' in dirs:
+            dirs.remove('__pycache__')
+        for file in files:
+            file_list.append(os.path.join(root, file))
+    return file_list
+
+def clean_files(files):
+    return [file for file in files if not any(file.endswith(ext) for ext in extensions)]
+
+def count_tokens():
+    return 0
 
 def main():
     dir = os.getcwd()
     files = list_files(dir)
-    for file in files:
+    cleaned_files = clean_files(files)
+    for file in cleaned_files:
         print(file)
 
 if __name__ == '__main__':
--- settings.py
+++
@@ -1,5 +0,0 @@
-extensions = [
-    '.git', 
-    '.env', 
-    '.exe'
-]
