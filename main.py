b384a0de30d49fc4f15d1e4fa86aada4cfbfddbc
angusgee <maltawebdev@gmail.com>
2024-02-03 20:00:54 +0100

    Add delimiters and revamp main function
    - add function to delimit the texts so the AI can tell them apart
    - revamp the main function in a more procedural way as it was getting too confusing passing the functions into each other

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def list_files(dir):
     return file_paths
 
 # exclude file if extension is in list
-def clean_files(files):
+def remove_files(files):
     extensions = ['.git', '.gitignore', '.env', '.exe','.jpeg', '.jpg', '.png']
     return [file for file in files if not any(file.endswith(ext) for ext in extensions)]
 
@@ -31,10 +31,14 @@ def read_file(file_path):
 
 def remove_blank_rows(text):
     return '\n'.join([line for line in text.splitlines() if line.strip() != ''])
+
+def add_delimiters(text):
+    return f"{text}```"
         
 def count_tokens(text):
     tokens = re.findall(r'\b\w+\b|\S', text)
     return len(tokens)
+
 
 def get_user_input():
     return int(input("""
@@ -53,11 +57,6 @@ def get_user_input():
         """))
 
 def main():
-    dir = os.getcwd()
-    cleaned_files_list = clean_files(list_files(dir))
-    for file in cleaned_files_list:
-        filename = file.split('/')[-1]
-        print(f"count of tokens for {filename}: {count_tokens(remove_blank_rows(read_file(file)))}")
     while True:
         try:
             user_prompt = get_user_input()
@@ -67,7 +66,29 @@ def main():
                 print('please choose a valid option 1-5')
         except ValueError:
             print('please enter a number')
-            
+    dir = os.getcwd()
+    file_list = remove_files(list_files(dir))
+    prompt_text = ''
+    total_token_count = 0
+    for file in file_list:
+        filename = file.split('/')[-1]
+        blank_rows_removed = remove_blank_rows(read_file(file))
+        text_with_delimiters = add_delimiters(blank_rows_removed)
+        token_count =  count_tokens(text_with_delimiters)
+        total_token_count += token_count
+        print(f"count of tokens for {filename}: {token_count}")
+        file_text = f"\n{filename}:\n {text_with_delimiters}"
+        prompt_text += file_text
+    print(prompt_text)
+    print(f"total tokens: {total_token_count}")
+        
+        
+
+    # concatenate the delimited strings, filepaths, and text together
+    # add the prompt based on user selection
+    # output to file
+    # copy to clipboard
+                
 if __name__ == '__main__':
     main()
     
